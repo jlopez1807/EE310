281af0a7c0a60b6c8c5f787f7152bbfb915678b3
jlopez1807 <146575920+jlopez1807@users.noreply.github.com>
2024-04-23 17:18:34 -0700

    Update Int_w_sens.c
    Removed definitions and program constants and included a new header file containing those definitions/variables/values.

--- Assignments/Int_w_sens.c
+++ Assignments/Int_w_sens.c
@@ -30,32 +30,8 @@
 #include <stdio.h>
 #include "Int_w_sens_config.h"
 #include "Int_w_sens_functions.h"
+#include "Int_w_sens_def.h"
 #include "C:/Program Files/Microchip/xc8/v2.46/pic/include/proc/pic18f47k42.h"
-/*-------------------------------
- Definitions
----------------------------------*/
-#define _XTAL_FREQ 4000000                 // Fosc  frequency for _delay()  library
-#define FCY    _XTAL_FREQ/4
-#define EM_SW PORTBbits.RB0
-#define Num_1 PORTBbits.RB1
-#define Num_2 PORTBbits.RB2
-#define SYS_LED PORTBbits.RB3
-#define EM_LED PORTBbits.RB4
-#define enter_button PORTBbits.RB5
-#define buzzer PORTBbits.RB6
-#define relay_mod PORTBbits.RB7
-#define Secret_Code 0x30
-/*---------------------------------------
- Program Inputs / Function Initialization
-------------------------------------------*/
-    unsigned char a;            // Placeholder variable
-    unsigned char count;        
-    unsigned char digit_1 __attribute ((address (0x30)));
-    unsigned char digit_2 __attribute ((address (0x34)));
-    unsigned char num_ent __attribute ((address (0x38)));
-    void seven_seg (unsigned char count1);
-    void store_1 (void);
-    void store_2 (void);
 /*-------------------------------
  Main Program
 ---------------------------------*/    
